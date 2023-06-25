3d8b688d36cde150f1abd7619127644714b925ee
VedantGonnade <vedantgonnade@outlook.com>
2023-06-25 21:37:01 +0200

    refactoring

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,23 +1,18 @@
 function generateBars(){
-    let i = 0;
-    const randomNumbers = [];
     const barsId = document.getElementById('bars');
     barsId.innerHTML = '';
     
-    while (i < 100) {
-        const randomNumber = Math.floor(Math.random() *100);
-        randomNumbers.push(randomNumber);
-        
+    for (let i = 0; i < 100; i++) {
+        const randomNumber = Math.floor(Math.random() * 100);
         const bar = document.createElement('div');
         bar.classList.add('bar-style');
         bar.style.height = randomNumber * 5 + 'px';
         barsId.appendChild(bar);
-        i++;
     }
     
     barsId.classList.add('bars-style');
-    
 }
+
 const getNewArrayCommand = document.getElementById('new-array-button')
 getNewArrayCommand.addEventListener("click", generateBars);
 generateBars();
