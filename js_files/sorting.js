a55a0d5b96de3d87acd4684df70d17c929045bb5
VedantGonnade <vedantgonnade@outlook.com>
2023-06-25 21:19:44 +0200

    added logic to generate bars whenever the new array button is clicked

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,18 +1,28 @@
-let randomNumbers = [];
-let i = 0;
-const barsId = document.getElementById('bars');
-
-while (i < 100) {
-    const randomNumber = Math.floor(Math.random() *100);
-    randomNumbers.push(randomNumber);
-    i++;
-    const bar = document.createElement('div');
-    bar.classList.add('bar-style');
-    bar.style.height = randomNumber * 5 + 'px';
-    barsId.appendChild(bar);
+function generateBars(){
+    let i = 0;
+    const randomNumbers = [];
+    const barsId = document.getElementById('bars');
+    barsId.innerHTML = '';
+    
+    while (i < 100) {
+        const randomNumber = Math.floor(Math.random() *100);
+        randomNumbers.push(randomNumber);
+        
+        const bar = document.createElement('div');
+        bar.classList.add('bar-style');
+        bar.style.height = randomNumber * 5 + 'px';
+        barsId.appendChild(bar);
+        i++;
+    }
+    
+    barsId.classList.add('bars-style');
+    
 }
-// i = 0;
-barsId.classList.add('bars-style');
+const getNewArrayCommand = document.getElementById('new-array-button')
+getNewArrayCommand.addEventListener("click", generateBars);
+generateBars();
+
+
 
 
 
