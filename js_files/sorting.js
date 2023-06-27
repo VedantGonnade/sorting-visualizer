165cc92d3c789d89ae55ec8382ff258ff5b9fc98
VedantGonnade <vedantgonnade@outlook.com>
2023-06-27 21:04:45 +0200

    added code to change number of bars dynamically

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -1,23 +1,33 @@
-function generateBars(){
+function generateBars(numberOfBars){
     const barsId = document.getElementById('bars');
     barsId.innerHTML = '';
     
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < numberOfBars; i++) {
         const randomNumber = Math.floor(Math.random() * 100);
         const bar = document.createElement('div');
         bar.id = 'bar-style-id-' + i;
         bar.classList.add('bar-style');
-        bar.style.height = randomNumber * 5 + 'px';
+        bar.style.height = randomNumber * 4 + 'px';
         barsId.appendChild(bar);
     }
     
     barsId.classList.add('bars-style');
 }
 
-const getNewArrayCommand = document.getElementById('new-array-button')
-getNewArrayCommand.addEventListener("click", generateBars);
-generateBars();
+function createBars() {
+    let arraySize = document.querySelector("#number-of-bar");
+    let numberOfBars = arraySize.value;
+    generateBars(numberOfBars)
+}
 
+
+const getNewArrayCommand = document.getElementById('new-array-button')
+getNewArrayCommand.addEventListener("click", createBars);
+
+let arraySize = document.querySelector("#number-of-bar");
+arraySize.addEventListener("input", createBars)
+
+createBars();
 
 
 
